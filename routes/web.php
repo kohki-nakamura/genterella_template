167012461fad4db84a4d5739d11671f2d18ecff5
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-11-13 00:02:07 +0900

    add admin directory

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/admin/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => 'auth'], function () {
+    // Dashboard
+    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+});
